32280df56d08f881e3b0cb315252cee564447e6d
alicia <sleechengn@live.cn>
2025-08-09 00:05:38 +0000

    保存程序 调试（可能有错误）

--- pvevm-hooks/vm-hook.sh
+++ pvevm-hooks/vm-hook.sh
@@ -5,20 +5,20 @@ SELECT=$2
 
 if [ $SELECT == "pre-start" ]; then
 	echo "pre-start-start"
-	/opt/pvevm-hooks/vm-start.sh $VMID $SELECT
+	$(dirname $0)/vm-start.sh $VMID $SELECT
 fi
 
 if [ $SELECT == "post-start" ]; then
 	echo "post-start-start"
-        /opt/pvevm-hooks/vm-run.sh $VMID $SELECT
+        $(dirname $0)/vm-run.sh $VMID $SELECT
 fi
 
 if [ $SELECT == "pre-stop" ]; then
         echo "pre-stop-start"
-        /opt/pvevm-hooks/vm-pre-stop.sh $VMID $SELECT
+        $(dirname $0)/vm-pre-stop.sh $VMID $SELECT
 fi
 
 if [ $SELECT == "post-stop" ]; then
 	echo "post-stop-start"
-        /opt/pvevm-hooks/vm-stop.sh $VMID $SELECT
+        $(dirname $0)/vm-stop.sh $VMID $SELECT
 fi
