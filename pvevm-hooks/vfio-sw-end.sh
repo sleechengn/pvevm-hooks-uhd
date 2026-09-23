a07be4d217d790d7f9351154c5fff441586fdf4b
lee <sleechengn@live.cn>
2026-09-23 14:31:39 +0800

    sav

--- pvevm-hooks/vfio-sw-end.sh
+++ pvevm-hooks/vfio-sw-end.sh
@@ -7,3 +7,15 @@ if systemctl status bluetooth > /dev/null 2>&1; then
     sleep 1
     systemctl start bluetooth
 fi
+
+if id sa > /dev/null 2>&1; then
+    if systemctl --user -M sa@ status pipewire > /dev/null 2>&1; then
+        systemctl --user -M sa@ start pipewire
+    fi
+    if systemctl --user -M sa@ status pipewire-pulse > /dev/null 2>&1; then
+        systemctl --user -M sa@ start pipewire-pulse
+    fi
+    if systemctl --user -M sa@ status wireplumber > /dev/null 2>&1; then
+        systemctl --user -M sa@ start wireplumber
+    fi
+fi
--- pvevm-hooks/vfio-sw-start.sh
+++ pvevm-hooks/vfio-sw-start.sh
@@ -7,3 +7,15 @@ if systemctl status bluetooth > /dev/null 2>&1; then
     sleep 1
     modprobe -r btusb
 fi
+
+if id sa > /dev/null 2>&1; then
+    if systemctl --user -M sa@ status pipewire > /dev/null 2>&1; then
+        systemctl --user -M sa@ stop pipewire
+    fi
+    if systemctl --user -M sa@ status pipewire-pulse > /dev/null 2>&1; then
+        systemctl --user -M sa@ stop pipewire-pulse
+    fi
+    if systemctl --user -M sa@ status wireplumber > /dev/null 2>&1; then
+        systemctl --user -M sa@ stop wireplumber
+    fi
+fi
