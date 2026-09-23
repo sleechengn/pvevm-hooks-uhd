b1fc65cf6dc989cdaeb9fb90b5cc9525c38e3362
lee <sleechengn@live.cn>
2026-09-23 14:58:37 +0800

    sav

--- pvevm-hooks/vfio-sw-end.sh
+++ pvevm-hooks/vfio-sw-end.sh
@@ -9,16 +9,16 @@ if systemctl status bluetooth > /dev/null 2>&1; then
 fi
 
 if id sa > /dev/null 2>&1; then
-    if systemctl --user -M sa@ status niri > /dev/null 2>&1; then
+    if [ "$(systemctl --user -M sa@ is-active niri)" == "inactive" ]; then
         systemctl --user -M sa@ start niri
     fi
-    if systemctl --user -M sa@ status pipewire > /dev/null 2>&1; then
+    if [ "$(systemctl --user -M sa@ is-active pipewire)" == "inactive" ]; then
         systemctl --user -M sa@ start pipewire
     fi
-    if systemctl --user -M sa@ status pipewire-pulse > /dev/null 2>&1; then
+    if [ "$(systemctl --user -M sa@ is-active pipewire-pulse)" == "inactive" ]; then
         systemctl --user -M sa@ start pipewire-pulse
     fi
-    if systemctl --user -M sa@ status wireplumber > /dev/null 2>&1; then
+    if [ "$(systemctl --user -M sa@ is-active wireplumber)" == "inactive" ]; then
         systemctl --user -M sa@ start wireplumber
     fi
 fi
