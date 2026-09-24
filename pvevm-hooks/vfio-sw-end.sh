5022db0863065db72d091d67ee9b42d60aea0f87
lee <sleechengn@live.cn>
2026-09-24 15:51:58 +0800

    sav

--- pvevm-hooks/vfio-sw-end.sh
+++ pvevm-hooks/vfio-sw-end.sh
@@ -9,16 +9,24 @@ if systemctl status bluetooth > /dev/null 2>&1; then
 fi
 
 if id sa > /dev/null 2>&1; then
-    if [ "$(systemctl --user -M sa@ is-active niri)" == "inactive" ]; then
-        systemctl --user -M sa@ start niri
+
+    if [ -e "/tmp/sa_services" ]; then
+        while read serviceName; do
+            echo "start $serviceName"
+            systemctl --user -M sa@ start $serviceName
+        done < "/tmp/sa_services"
     fi
-    if [ "$(systemctl --user -M sa@ is-active pipewire)" == "inactive" ]; then
-        systemctl --user -M sa@ start pipewire
-    fi
-    if [ "$(systemctl --user -M sa@ is-active pipewire-pulse)" == "inactive" ]; then
-        systemctl --user -M sa@ start pipewire-pulse
-    fi
-    if [ "$(systemctl --user -M sa@ is-active wireplumber)" == "inactive" ]; then
-        systemctl --user -M sa@ start wireplumber
-    fi
+
+    #if [ "$(systemctl --user -M sa@ is-active niri)" == "inactive" ]; then
+    #    systemctl --user -M sa@ start niri
+    #fi
+    #if [ "$(systemctl --user -M sa@ is-active pipewire)" == "inactive" ]; then
+    #    systemctl --user -M sa@ start pipewire
+    #fi
+    #if [ "$(systemctl --user -M sa@ is-active pipewire-pulse)" == "inactive" ]; then
+    #    systemctl --user -M sa@ start pipewire-pulse
+    #fi
+    #if [ "$(systemctl --user -M sa@ is-active wireplumber)" == "inactive" ]; then
+    #    systemctl --user -M sa@ start wireplumber
+    #fi
 fi
--- pvevm-hooks/vfio-sw-start.sh
+++ pvevm-hooks/vfio-sw-start.sh
@@ -8,17 +8,23 @@ if systemctl status bluetooth > /dev/null 2>&1; then
     modprobe -r btusb
 fi
 
+rm -rf /tmp/sa_services
+
 if id sa > /dev/null 2>&1; then
     if systemctl --user -M sa@ status niri > /dev/null 2>&1; then
         systemctl --user -M sa@ stop niri
+        echo "niri" >> /tmp/sa_services
     fi
     if systemctl --user -M sa@ status pipewire > /dev/null 2>&1; then
         systemctl --user -M sa@ stop pipewire
+        echo "pipewire" >> /tmp/sa_services
     fi
     if systemctl --user -M sa@ status pipewire-pulse > /dev/null 2>&1; then
         systemctl --user -M sa@ stop pipewire-pulse
+        echo "pipewire-pulse" >> /tmp/sa_services
     fi
-    if systemctl --user -M sa@ status wireplumber > /dev/null 2>&1; then
+    if systemctl --user -M sa@ status  > /dev/null 2>&1; then
         systemctl --user -M sa@ stop wireplumber
+        echo "wireplumber" >> /tmp/sa_services
     fi
 fi
